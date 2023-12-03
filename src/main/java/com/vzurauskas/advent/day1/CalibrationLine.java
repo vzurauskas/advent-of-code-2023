a9eb5ccb3f4b33b32c40dda75f50ca4e3c562d89
Vytautas Žurauskas <zurauskas.vytautas@gmail.com>
2023-12-03 20:52:26 +0200

    Partial solution to day 1 part 2.

--- src/main/java/com/vzurauskas/advent/day1/CalibrationLine.java
+++ src/main/java/com/vzurauskas/advent/day1/CalibrationLine.java
@@ -1,7 +1,9 @@
 package com.vzurauskas.advent.day1;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class CalibrationLine {
     private final String line;
@@ -11,7 +13,7 @@ public final class CalibrationLine {
     }
 
     public int value() {
-        List<String> chars = line.chars()
+        List<String> chars = withNumbersFixed().chars()
             .filter(Character::isDigit)
             .mapToObj(c -> String.valueOf((char) c))
             .map(String::valueOf)
@@ -19,5 +21,34 @@ public final class CalibrationLine {
         String first = chars.get(0);
         String last = chars.get(chars.size() - 1);
         return Integer.parseInt(first + last);
+    }
+
+    private String withNumbersFixed() {
+        String previous = line;
+        String replaced = withFirstReplaced(previous);
+        while (!previous.equals(replaced)) {
+            previous = replaced;
+            replaced = withFirstReplaced(previous);
+        }
+        return replaced;
+    }
+
+    private static String withFirstReplaced(String line) {
+        List<ReplacementCandidate> candidates = Stream.of(
+            new ReplacementCandidate(line, "one", 1),
+            new ReplacementCandidate(line, "two", 2),
+            new ReplacementCandidate(line, "three", 3),
+            new ReplacementCandidate(line, "four", 4),
+            new ReplacementCandidate(line, "five", 5),
+            new ReplacementCandidate(line, "six", 6),
+            new ReplacementCandidate(line, "seven", 7),
+            new ReplacementCandidate(line, "eight", 8),
+            new ReplacementCandidate(line, "nine", 9)
+        )
+            .filter(candidate -> line.contains(candidate.word()))
+            .collect(Collectors.toList());
+        return candidates.isEmpty()
+            ? line
+            : Collections.max(candidates).apply();
     }
 }
--- src/test/java/com/vzurauskas/advent/day1/CalibrationLineTest.java
+++ src/test/java/com/vzurauskas/advent/day1/CalibrationLineTest.java
@@ -7,33 +7,56 @@ final class CalibrationLineTest {
 
     @Test
     void decodesInput1() {
-        assertEquals(
-            12,
-            new CalibrationLine("1abc2").value()
-        );
+        assertEquals(12, new CalibrationLine("1abc2").value());
     }
 
     @Test
     void decodesInput2() {
-        assertEquals(
-            38,
-            new CalibrationLine("pqr3stu8vwx").value()
-        );
+        assertEquals(38, new CalibrationLine("pqr3stu8vwx").value());
     }
 
     @Test
     void decodesInput3() {
-        assertEquals(
-            15,
-            new CalibrationLine("a1b2c3d4e5f").value()
-        );
+        assertEquals(15, new CalibrationLine("a1b2c3d4e5f").value());
     }
 
     @Test
     void decodesInput4() {
-        assertEquals(
-            77,
-            new CalibrationLine("treb7uchet").value()
-        );
+        assertEquals(77, new CalibrationLine("treb7uchet").value());
+    }
+
+    @Test
+    void decodesLineWithWords1() {
+        assertEquals(29, new CalibrationLine("two1nine").value());
+    }
+
+    @Test
+    void decodesLineWithWords2() {
+        assertEquals(83, new CalibrationLine("eightwothree").value());
+    }
+
+    @Test
+    void decodesLineWithWords3() {
+        assertEquals(13, new CalibrationLine("abcone2threexyz").value());
+    }
+
+    @Test
+    void decodesLineWithWords4() {
+        assertEquals(24, new CalibrationLine("xtwone3four").value());
+    }
+
+    @Test
+    void decodesLineWithWords5() {
+        assertEquals(42, new CalibrationLine("4nineeightseven2").value());
+    }
+
+    @Test
+    void decodesLineWithWords6() {
+        assertEquals(14, new CalibrationLine("zoneight234").value());
+    }
+
+    @Test
+    void decodesLineWithWords7() {
+        assertEquals(76, new CalibrationLine("7pqrstsixteen").value());
     }
 }
